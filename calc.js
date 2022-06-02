9bb7a8f6267aad97619a2c40dbefb5091c046ef5
imdudder <79345007+imdudder@users.noreply.github.com>
2022-06-01 21:14:17 -0700

    Decoupled conversions from event handlers

--- calc.js
+++ calc.js
@@ -1,136 +1,129 @@
-// Binary To Decimal
-var binToDecInput = document.getElementById('binToDecInput');
-var binToDecOutput = document.getElementById('binToDecOutput');
+// Binary To Decimal Listener
 var binToDecForm = document.getElementById('bin-to-dec-form');
-binToDecForm.addEventListener('submit', convertBinToDec);
+binToDecForm.addEventListener('submit', binToDecHandler);
 
-// Decimal to Binary
-var decToBinInput = document.getElementById('decToBinInput');
-var decToBinOutput = document.getElementById('decToBinOutput');
+// Decimal to Binary Listener
 var decToBinForm = document.getElementById('dec-to-bin-form');
-decToBinForm.addEventListener('submit', convertDecToBin);
+decToBinForm.addEventListener('submit', decToBinHandler);
 
-
-// Hexadecimal to Decimal
-var hexToDecInput = document.getElementById('hexToDecInput');
-var hexToDecOutput = document.getElementById('hexToDecOutput');
+// Hexadecimal to Decimal Listener
 var hexToDecForm = document.getElementById('hex-to-dec-form');
-hexToDecForm.addEventListener('submit', convertHexToDec);
+hexToDecForm.addEventListener('submit', hexToDecHandler);
 
-// Decimal to Hexadecimal
-var decToHexInput = document.getElementById('decToHexInput');
-var decToHexOutput = document.getElementById('decToHexOutput');
+// Decimal to Hexadecimal Listener
 var decToHexForm = document.getElementById('dec-to-hex-form');
-decToHexForm.addEventListener('submit', convertDecToHex);
+decToHexForm.addEventListener('submit', decToHexHandler);
 
 
 //===================== Binary to Decimal Section ====================
-function convertBinToDec(event) {
-  // Validate Inputs
+function binToDecHandler(event) {
+	var binToDecInput = document.getElementById('binToDecInput');
+	var binToDecOutput = document.getElementById('binToDecOutput');
   if (binToDecInput.value) {
-    var binVal = parseInt(binToDecInput.value);
-    var decResult = 0;
-
-    // Convert binary to decimal
-    for (let multiplier = 1; binVal > 0; multiplier *= 2) {
-      let temp = parseInt(binVal % 2);
-      decResult += (temp * multiplier);
-      binVal = parseInt(binVal/10);
-    }
-
-    // Return value
-    binToDecOutput.value = decResult;
-    event.preventDefault(); //Turn off page refresh
+		binToDecOutput.value = binaryToDecimal(parseInt(binToDecInput.value));
   }
+	event.preventDefault();
+}
+
+function binaryToDecimal(binVal) {
+	var decVal = 0;
+	for (let multiplier = 1; binVal > 0; multiplier *= 2) {
+		let remainder = parseInt(binVal % 2);
+		decVal += (remainder * multiplier);
+		binVal = parseInt(binVal / 10);
+	}
+	return decVal;
 }
 
 
 //===================== Decimal to Binary Section ====================
-function convertDecToBin(event) {
-  // Validate Inputs
+function decToBinHandler(event) {
+	var decToBinInput = document.getElementById('decToBinInput');
+	var decToBinOutput = document.getElementById('decToBinOutput');
   if (decToBinInput.value) {
-    var decVal = parseInt(decToBinInput.value);
-    var binResult = "";
-    while (decVal > 0) {
-      if (decVal % 2 == 0) {
-        binResult = "0" + binResult;
-      }
-      else {
-        binResult = "1" + binResult;
-      }
-      decVal = parseInt(decVal/2);
-    }
-    decToBinOutput.value = binResult;
+		decToBinOutput.value = decimalToBinary(parseInt(decToBinInput.value));
   }
   event.preventDefault();
+}
+
+function decimalToBinary(decVal) {
+	var binVal = "";
+	while (decVal > 0) {
+		binVal = (decVal % 2 == 0) ? "0" + binVal : "1" + binVal;
+		decVal = parseInt(decVal/2);
+	}
+	return binVal;
 }
 
 
 // ===================== Hex to Decimal Section ====================
-function convertHexToDec(event) {
-  // Validate Inputs
+function hexToDecHandler(event) {
+	var hexToDecInput = document.getElementById('hexToDecInput');
+	var hexToDecOutput = document.getElementById('hexToDecOutput');
   if (hexToDecInput.value) {
-    var hexVal = String(hexToDecInput.value);
-    var decResult = 0;
-
-    // Convert hex to decimal
-    let multiplier = 1;
-    for (let hexDigitIndex = hexVal.length - 1; hexDigitIndex >= 0; hexDigitIndex--) {
-      let hexDigit = 0;
-      if (hexVal[hexDigitIndex] >= '0' && hexVal[hexDigitIndex] <= '9') {
-        hexDigit = parseInt(hexVal[hexDigitIndex]);
-      }
-      else if (hexVal[hexDigitIndex] >= 'A' && hexVal[hexDigitIndex] <= 'F') {
-        hexDigit = hexVal.charCodeAt(hexDigitIndex) - 55;
-      }
-      else if (hexVal[hexDigitIndex] >= 'a' && hexVal[hexDigitIndex] <= 'f') {
-        hexDigit = hexVal.charCodeAt(hexDigitIndex) - 87;
-      }
-      decResult += (hexDigit * multiplier);
-      multiplier *= 16;
-    }
-
-    // Return value
-    hexToDecOutput.value = decResult;
-    event.preventDefault(); // Turn off page refresh
+		hexToDecOutput.value = hexToDecimal(String(hexToDecInput.value));
   }
+	event.preventDefault(); // Turn off page refresh
+}
+
+function hexToDecimal(hexVal) {
+	let multiplier = 1;
+	let decVal = 0;
+	for (let hexIndex = hexVal.length - 1; hexIndex >= 0; hexIndex--) {
+		let hexDigit = 0;
+		if (hexVal[hexIndex] >= '0' && hexVal[hexIndex] <= '9') {
+			hexDigit = parseInt(hexVal[hexIndex]);
+		}
+		else if (hexVal[hexIndex] >= 'A' && hexVal[hexIndex] <= 'F') {
+			hexDigit = hexVal.charCodeAt(hexIndex) - 55;
+		}
+		else if (hexVal[hexIndex] >= 'a' && hexVal[hexIndex] <= 'f') {
+			hexDigit = hexVal.charCodeAt(hexIndex) - 87;
+		}
+		decVal += (hexDigit * multiplier);
+		multiplier *= 16;
+	}
+	return decVal;
 }
 
 
 // ===================== Decimal to Hex Section ====================
-function convertDecToHex(event) {
-  // Validate Inputs
+function decToHexHandler(event) {
+	var decToHexInput = document.getElementById('decToHexInput');
+	var decToHexOutput = document.getElementById('decToHexOutput');
   if (decToHexInput.value) {
-    var decVal = parseInt(decToHexInput.value);
-    var hexResult = "";
-    while (decVal > 0) {
-      let remainder = decVal % 16;
-      switch(remainder) {
-        case 10:
-          hexResult = "A" + hexResult;
-          break;
-        case 11:
-          hexResult = "B" + hexResult;
-          break;
-        case 12:
-          hexResult = "C" + hexResult;
-          break;
-        case 13:
-          hexResult = "D" + hexResult;
-          break;
-        case 14:
-          hexResult = "E" + hexResult;
-          break;
-        case 15:
-          hexResult = "F" + hexResult;
-          break;
-        default:
-          hexResult = String(remainder) + hexResult;
-      }
-      decVal = parseInt(decVal / 16);
-    }
-
-    decToHexOutput.value = String(hexResult);
+		decToHexOutput.value = decimalToHex(parseInt(decToHexInput.value));
   }
   event.preventDefault();
+}
+
+function decimalToHex(decVal) {
+	var hexVal = "";
+	while (decVal > 0) {
+		let remainder = decVal % 16;
+		switch(remainder) {
+			case 10:
+				hexVal = "A" + hexVal;
+				break;
+			case 11:
+				hexVal = "B" + hexVal;
+				break;
+			case 12:
+				hexVal = "C" + hexVal;
+				break;
+			case 13:
+				hexVal = "D" + hexVal;
+				break;
+			case 14:
+				hexVal = "E" + hexVal;
+				break;
+			case 15:
+				hexVal = "F" + hexVal;
+				break;
+			default:
+				hexVal = String(remainder) + hexVal;
+		}
+		decVal = parseInt(decVal / 16);
+	}
+	return hexVal;
 }
